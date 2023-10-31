c03af44073c86f4e888559b47ef3a496e092a0b8
Zanon22 <felipezanon33@gmail.com>
2023-10-31 16:38:19 -0300

    novo

--- aulas/aula 3.py
+++
@@ -1,9 +0,0 @@
-import pandas as pd
-import sqlite3
-
-conexao = sqlite3.connect("../chinook.db")
-
-tabela_clientes = pd.read_sql("SELECT * FROM customers", conexao)
-print(tabela_clientes)
-
-conexao.close()
